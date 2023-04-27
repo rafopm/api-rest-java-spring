c26e541a1af6e0ae5fe99dc6affeff2767aa4964
rafopm <rafopm@gmail.com>
2023-04-27 13:39:50 -0500

    Initial commit

---
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -0,0 +1,25 @@
+package med.voll.api.controller;
+
+import jakarta.validation.Valid;
+import med.voll.api.medico.DatosRegistroMedico;
+import med.voll.api.medico.Medico;
+import med.voll.api.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/medicos")
+public class MedicoController {
+
+    @Autowired  //Forma abreviada
+    private MedicoRepository medicoRepository;
+
+    @PostMapping
+    public void registrarMedico(@RequestBody @Valid DatosRegistroMedico datosRegistroMedico){
+    medicoRepository.save(new Medico(datosRegistroMedico));
+        System.out.println(datosRegistroMedico);
+    }
+}
